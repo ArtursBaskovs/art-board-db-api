ea5e796382dc1fb0ade10d0df0a12b0461e6746a
unknown <12arturb34@gmail.com>
2025-07-30 20:00:06 +0300

    fixes for render.com

--- index.php
+++ index.php
@@ -19,7 +19,8 @@ header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PATCH, PUT, DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-require_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
 
 use App\BoardController;
 use App\BoardGateway;
